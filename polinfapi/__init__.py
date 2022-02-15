7cf152bb1e0daec4bfcc765217b66cd1ed6bed0b
Yan Kurbatov <57015939+deknowny@users.noreply.github.com>
2022-02-15 12:15:59 +0300

    Add import of TaskModel

--- polinfapi/__init__.py
+++ polinfapi/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.0a0"
 
 from polinfapi.client import BaseSyncClient, RequestsClient
-from polinfapi.models.tasks import Task1ModelsAnalysis
 from polinfapi.utils import download_image
+from polinfapi.models.base import TaskModel
